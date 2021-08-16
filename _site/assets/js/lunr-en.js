32e16ab23d81f9e7855f2d36cc74fba76278d3ed
Bruno Neves <brunoneves@Brunos-MacBook-Air.local>
2021-08-16 16:59:32 +0100

    m

--- _site/assets/js/lunr-en.js
+++ _site/assets/js/lunr-en.js
@@ -83,20 +83,11 @@ var idx = lunr(function () {
     
   
     idx.add({
-      title: "B Trees",
-      excerpt: "\n",
-      categories: [],
-      tags: [],
-      id: 8
-    });
-    
-  
-    idx.add({
       title: "Async Javascript",
       excerpt: "Computer programs, such as scientific simulations, are CPU Bound: they run continuously, with no pause, until they have computed their...",
       categories: [],
       tags: [],
-      id: 9
+      id: 8
     });
     
   
@@ -105,16 +96,7 @@ var idx = lunr(function () {
       excerpt: "Caching plays a critical role in high load applications, and it’s used almost everywhere on the web, from static resources...",
       categories: [],
       tags: [],
-      id: 10
-    });
-    
-  
-    idx.add({
-      title: "CPU Bound",
-      excerpt: "\n",
-      categories: [],
-      tags: [],
-      id: 11
+      id: 9
     });
     
   
@@ -123,7 +105,7 @@ var idx = lunr(function () {
       excerpt: "Node.js is a non-blocking single thread architecture that works great for applications handling a moderate number of requests per second...",
       categories: [],
       tags: [],
-      id: 12
+      id: 10
     });
     
   
@@ -132,7 +114,7 @@ var idx = lunr(function () {
       excerpt: "Neurons A neuron is the basic unit of a neural network. A neuron takes inputs, does some math with them,...",
       categories: [],
       tags: [],
-      id: 13
+      id: 11
     });
     
   
@@ -141,16 +123,7 @@ var idx = lunr(function () {
       excerpt: "Tasks that are trivial in traditional synchronous programming can become more complicated when applied to asynchronous programming. A typical example...",
       categories: [],
       tags: [],
-      id: 14
-    });
-    
-  
-    idx.add({
-      title: "Multivariable Calculus",
-      excerpt: "For multivariable integrals, the true story starts with a change of coordinates. Let \\(f : D ⊂ R ^ n...",
-      categories: [],
-      tags: [],
-      id: 15
+      id: 12
     });
     
   
@@ -166,7 +139,7 @@ var store = [
       
       {
         "title": "Pokemons,Birthdays and Cryptography",
-        "url": "http://localhost:4000/cableBlog/Pokemons,Birthdays-and-Cryptography/",
+        "url": "/cableBlog/Pokemons,Birthdays-and-Cryptography/",
         "excerpt": "Supose you’re walking on the streets hunting Pokemons in \\(Pokemon Go\\). Your objective is to collect \\(N\\) different types of...",
         "teaser":
           
@@ -177,7 +150,7 @@ var store = [
       
       {
         "title": "Currency Exchange and Negative Cycles",
-        "url": "http://localhost:4000/cableBlog/BellmanFord/",
+        "url": "/cableBlog/BellmanFord/",
         "excerpt": "I really enjoy graph problems, because most of the problems the difficulty lies not in the knowledge of various graph...",
         "teaser":
           
@@ -188,7 +161,7 @@ var store = [
       
       {
         "title": "Quake III and the Fast Inverse Square Root",
-        "url": "http://localhost:4000/cableBlog/Quake-and-the-Fast-Inverse-Square-Root/",
+        "url": "/cableBlog/Quake-and-the-Fast-Inverse-Square-Root/",
         "excerpt": "Given a number \\(x\\) ,write a program that calculates the inverse square root: \\[y = \\frac{1}{\\sqrt{x}}\\] Well, it’s not that...",
         "teaser":
           
@@ -199,7 +172,7 @@ var store = [
       
       {
         "title": "Computational Geometry in C++",
-        "url": "http://localhost:4000/cableBlog/Computational-Geometry-in-C++/",
+        "url": "/cableBlog/Computational-Geometry-in-C++/",
         "excerpt": "In this post i just want to make a compilation of useful C++ computational geometry algorithms used in Competitive Programming....",
         "teaser":
           
@@ -210,7 +183,7 @@ var store = [
       
       {
         "title": "Optimizing Code Performance and control flow with Async",
-        "url": "http://localhost:4000/cableBlog/Optimizing-Code-Performance-and-control-flow-with-Async/",
+        "url": "/cableBlog/Optimizing-Code-Performance-and-control-flow-with-Async/",
         "excerpt": "Async handling its what makes Node.js a powerful tool but also a confusing one. With C++, Java, C# and other...",
         "teaser":
           
@@ -221,7 +194,7 @@ var store = [
       
       {
         "title": "Node.js and MySQL",
-        "url": "http://localhost:4000/cableBlog/Node.js-and-MySQL/",
+        "url": "/cableBlog/Node.js-and-MySQL/",
         "excerpt": "Node.js is able to connect to MySQL database servers, but support for this is not built in. We need to...",
         "teaser":
           
@@ -232,7 +205,7 @@ var store = [
       
       {
         "title": "Don't abuse the Spread Operator",
-        "url": "http://localhost:4000/cableBlog/Don't-abuse-the-spread-operator/",
+        "url": "/cableBlog/Don't-abuse-the-spread-operator/",
         "excerpt": "Here I was on my spare time doing some problems on Binary Search and I was doing this easy one...",
         "teaser":
           
@@ -243,7 +216,7 @@ var store = [
       
       {
         "title": "Python & Flask",
-        "url": "http://localhost:4000/cableBlog/Python-&-Flask/",
+        "url": "/cableBlog/Python-&-Flask/",
         "excerpt": "To begin: sudo pip install flask Flask is a good choice for a REST API because it is: Written in...",
         "teaser":
           
@@ -253,19 +226,8 @@ var store = [
     
       
       {
-        "title": "B Trees",
-        "url": "http://localhost:4000/cableBlog/B-Trees/",
-        "excerpt": "\n",
-        "teaser":
-          
-            null
-          
-      },
-    
-      
-      {
         "title": "Async Javascript",
-        "url": "http://localhost:4000/cableBlog/Async-Javascript/",
+        "url": "/cableBlog/Async-Javascript/",
         "excerpt": "Computer programs, such as scientific simulations, are CPU Bound: they run continuously, with no pause, until they have computed their...",
         "teaser":
           
@@ -276,7 +238,7 @@ var store = [
       
       {
         "title": "Asynchronous request batching and caching",
-        "url": "http://localhost:4000/cableBlog/Asynchronous-request-batching-and-caching/",
+        "url": "/cableBlog/Asynchronous-request-batching-and-caching/",
         "excerpt": "Caching plays a critical role in high load applications, and it’s used almost everywhere on the web, from static resources...",
         "teaser":
           
@@ -286,19 +248,8 @@ var store = [
     
       
       {
-        "title": "CPU Bound",
-        "url": "http://localhost:4000/cableBlog/CPU-Bound/",
-        "excerpt": "\n",
-        "teaser":
-          
-            null
-          
-      },
-    
-      
-      {
         "title": "Scalability and Architectural Patterns in Node.js",
-        "url": "http://localhost:4000/cableBlog/Scalibility-and-Architectural-Patterns/",
+        "url": "/cableBlog/Scalibility-and-Architectural-Patterns/",
         "excerpt": "Node.js is a non-blocking single thread architecture that works great for applications handling a moderate number of requests per second...",
         "teaser":
           
@@ -309,7 +260,7 @@ var store = [
       
       {
         "title": "A Simple Neural Net",
-        "url": "http://localhost:4000/cableBlog/A-Simple-Neural-Net/",
+        "url": "/cableBlog/A-Simple-Neural-Net/",
         "excerpt": "Neurons A neuron is the basic unit of a neural network. A neuron takes inputs, does some math with them,...",
         "teaser":
           
@@ -320,19 +271,8 @@ var store = [
       
       {
         "title": "Async Initialization in Node.js",
-        "url": "http://localhost:4000/cableBlog/Async-Initialization-in-Node.js/",
+        "url": "/cableBlog/Async-Initialization-in-Node.js/",
         "excerpt": "Tasks that are trivial in traditional synchronous programming can become more complicated when applied to asynchronous programming. A typical example...",
-        "teaser":
-          
-            null
-          
-      },
-    
-      
-      {
-        "title": "Multivariable Calculus",
-        "url": "http://localhost:4000/cableBlog/Multivariable-Calculus/",
-        "excerpt": "For multivariable integrals, the true story starts with a change of coordinates. Let \\(f : D ⊂ R ^ n...",
         "teaser":
           
             null
